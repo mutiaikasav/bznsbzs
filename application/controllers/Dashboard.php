8ddb355ffbc43a340be9f1e9c7bb06e2a1e20f67
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-07-27 09:29:51 +0700

    parse data dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,9 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
+
     public function index()
     {   
-        $data['content'] = 'dashboard';
-        $this->load->view('template');
+        $data['content'] = $this->helper->loadView('dashboard');
+        return $this->load->view('template', $data, TRUE);
     }
 }
