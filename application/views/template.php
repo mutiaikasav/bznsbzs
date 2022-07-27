d20887ffdb9b2bac1e42d8c57f8b3acfc4cea233
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-07-27 14:42:40 +0700

    template update

--- application/views/template.php
+++ application/views/template.php
@@ -12,17 +12,17 @@
     <title>CMS Baznas Bazis DKI Jakarta</title>
 
     <!-- Custom fonts for this template -->
-    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php base_url(); ?>/assets/css/sb-admin-2.css" rel="stylesheet">
     <!-- Custom styles for this page -->
-    <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
-    <script src="assets/js/demo/tinymce-demo.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/js/demo/tinymce-demo.min.js"></script>
 </head>
 <body id="page-top">
     <!-- Page Wrapper -->
@@ -57,21 +57,21 @@
     <?php $this->load->view('partials/logout-modal'); ?>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="assets/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="assets/js/sb-admin-2.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="assets/js/demo/datatables-demo.js"></script>
+    <script src="<?php base_url(); ?>/assets/js/demo/datatables-demo.js"></script>
 
     <!-- TinyMCE -->
     <script>
