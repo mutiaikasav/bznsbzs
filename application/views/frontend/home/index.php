ba3c473da85c36eadd74ac821c935479450abd4c
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-03 21:52:58 +0700

    frontend home (temporary)

---
+++ application/views/frontend/home/index.php
@@ -0,0 +1,386 @@
+<!-- Hero -->
+<section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner" role="listbox">          
+        <div class="carousel-item active" style="background-image: url('../../<?php base_url();?>/assets/frontend/img/DSC05210.JPG');">
+        <div class="carousel-container">
+            <div class="carousel-content animate__animated animate__fadeInUp">
+            <h2>Judul Artikel</h2>
+            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+            <div class="text-center"><a href="" class="btn-get-started">Lihat Selengkapnya</a></div>
+            </div>
+        </div>
+        </div>
+
+        <div class="carousel-item " style="background-image: url('../../<?php base_url();?>/assets/frontend/img/DSC05194.JPG');">
+        <div class="carousel-container">
+            <div class="carousel-content animate__animated animate__fadeInUp">
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+            <div class="text-center"><a href="" class="btn-get-started">Lihat Selengkapnya</a></div>
+            </div>
+        </div>
+        </div>
+
+        <div class="carousel-item " style="background-image: url('../../<?php base_url();?>/assets/frontend/img/DSC05241.JPG');">
+        <div class="carousel-container">
+            <div class="carousel-content animate__animated animate__fadeInUp">
+            <h2>Sequi ea ut et est quaerat</h2>
+            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+            <div class="text-center"><a href="" class="btn-get-started">Lihat Selengkapnya</a></div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+
+    <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+    </a>
+
+    <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+    </a>
+
+    <ol class="carousel-indicators" id="hero-carousel-indicators">
+        <li data-bs-target='#heroCarousel' data-bs-slide-to='1' class='active'></li>
+    </ol>
+
+    </div>
+</section>
+<!-- End Hero -->
+<div class="bg-img-bw">
+    <div class="widget container" style="margin-top:-20px;">
+    <br><br>
+    <center>
+        <h2 class="widget-title" style="color: yellow;font-weight: bold;">Kabar Baznas Bazis</h2>
+    </center>
+    <section class="section-property">
+        <div class="container">
+        <div class="row">
+            <div class="title-wrap text-right">
+            <div class="title-link">
+                <a href="kategori.html">Lihat lainnya
+                <span class="bi bi-chevron-right"></span>
+                </a>
+            </div>
+            </div>
+        </div>
+
+        <div id="property-carousel" class="swiper">
+            <div class="swiper-wrapper">      
+            <div class="carousel-item-b swiper-slide">
+                <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                    <img src="<?php base_url();?>/assets/frontend/img/DSC05194.JPG" alt="" class="img-a img-fluid lozad">
+                </div>
+                <div class="card-overlay">
+                    <div class="card-overlay-a-content">
+                    <div class="card-header-a">
+                        <h2 class="card-title-a">
+                        <a href="artikel.html">Lorem, ipsum dolor sit amet...</a>
+                        </h2>
+                    </div>
+                    <div class="card-body-a">
+                        <a href="#" class="btn-detail">Baca Selengkapnya
+                        <span class="bi bi-chevron-right"></span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="carousel-item-b swiper-slide">
+                <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                    <img src="<?php base_url();?>/assets/frontend/img/DSC05210.JPG" alt="" class="img-a img-fluid lozad">
+                </div>
+                <div class="card-overlay">
+                    <div class="card-overlay-a-content">
+                    <div class="card-header-a">
+                        <h2 class="card-title-a">
+                        <a href="artikel.html">Lorem, ipsum dolor sit amet...</a>
+                        </h2>
+                    </div>
+                    <div class="card-body-a">
+                        <a href="artikel.html" class="btn-detail">Baca Selengkapnya
+                        <span class="bi bi-chevron-right"></span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="carousel-item-b swiper-slide">
+                <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                    <img src="<?php base_url();?>/assets/frontend/img/DSC05241.JPG" alt="" class="img-a img-fluid lozad">
+                </div>
+                <div class="card-overlay">
+                    <div class="card-overlay-a-content">
+                    <div class="card-header-a">
+                        <h2 class="card-title-a">
+                        <a href="artikel.html">Lorem, ipsum dolor sit amet...</a>
+                        </h2>
+                    </div>
+                    <div class="card-body-a">
+                        <a href="artikel.html" class="btn-detail">Baca Selengkapnya
+                        <span class="bi bi-chevron-right"></span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="carousel-item-b swiper-slide">
+                <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                    <img src="<?php base_url();?>/assets/frontend/img/DSC05276.JPG" alt="" class="img-a img-fluid lozad">
+                </div>
+                <div class="card-overlay">
+                    <div class="card-overlay-a-content">
+                    <div class="card-header-a">
+                        <h2 class="card-title-a">
+                        <a href="artikel.html">Lorem, ipsum dolor sit amet....</a>
+                        </h2>
+                    </div>
+                    <div class="card-body-a">
+                        <a href="artikel.html" class="btn-detail" >Baca Selengkapnya</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="propery-carousel-pagination carousel-pagination"></div>      
+        </div>
+    </section>
+    </div>
+</div>
+<div>
+    <div class="widget">
+    <h2 class="widget-title container">Program</h2>
+    <section class="cards-wrapper" id="program">
+        <div class="card-grid-space">
+        <a href="program.html" class="card rounded-circle " style="--bg-img:url(../../<?php base_url();?>/assets/frontend/img/Jak-B-Bertaqwa-keagamaan-360x325.png)">
+            <div>
+                <h3>Jak B Bertaqwa</h3>
+            </div>
+        </a>
+        </div>
+        <div class="card-grid-space">
+        <a href="program.html" class="card rounded-circle " style="--bg-img:url(../../<?php base_url();?>/assets/frontend/img/Jak-B-Cerdas-Pendidikan-360x325.png)">
+            <div>
+            <h3>Jak B Cerdas</h3>
+            </div>
+        </a>
+        </div>
+        <div class="card-grid-space">
+        <a href="program.html" class="card rounded-circle " style="--bg-img:url(../../<?php base_url();?>/assets/frontend/img/Jak-B-Sehat-Kesehatan-360x325.png)">
+            <div>
+            <h3>Jak B Sehat</h3>
+            </div>
+        </a>
+        </div>
+        <div class="card-grid-space">
+        <a href="program.html" class="card rounded-circle " style="--bg-img:url(../../<?php base_url();?>/assets/frontend/img/Jak-B-Green-Lingkungan-360x325.png)">
+            <div>
+            <h3>Jak B Green</h3>
+            </div>
+        </a>
+        </div>
+        <div class="card-grid-space">
+        <a href="program.html" class="card rounded-circle " style="--bg-img:url(../../<?php base_url();?>/assets/frontend/img/Jak-B-Berdaya-Ekonomi-360x325.png)">
+            <div>
+            <h3>Jak B Berdaya</h3>
+            </div>
+        </a>
+        </div>
+    </section>
+    </div>
+    <!-- <h2>Galang Dana Terbaru</h2> -->
+    <div class="widget">
+    <section id="counts" class="counts section-bg">
+        <div class="container">
+        <div class="row counters">
+            <div class="col-lg-6 col-6 text-center">
+                <a onclick="show('mustahik')">
+                <img src="<?php base_url();?>/assets/frontend/img/penerima-zakat.png" alt="">
+                <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Mustahik</p>
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#exampleModal">Lihat Detail</button>
+                </a>
+            </div>
+            <div class="col-lg-6 col-6 text-center">
+            <a onclick="show('muzakki')">
+                <img src="<?php base_url();?>/assets/frontend/img/donatur.png" alt="">
+                <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Muzakki</p>
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#exampleModal">Lihat Detail</button>
+            </a>
+            </div>
+        </div>
+        </div>
+    </section>
+    </div>
+</div>
+<div class="content">
+    <div class="widget container bg-img-rad pt-3">
+    <h2 class="widget-title" style="color: white;">Galeri</h2>
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+        <div class="row" data-aos="fade-up">
+            <div class="col-lg- d-flex justify-content-center">
+            <ul id="portfolio-flters">
+                <li data-filter=".filter-photo" class="filter-active">Foto</li>
+                <li data-filter=".filter-video" style="color: white;">Video</li>
+            </ul>
+            <a class="color-white" href="galeri.html">Lihat Semua > </a>
+            </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
+                <a href="galeri-detail.html" style="text-decoration:none;">
+                <img src="<?php base_url();?>/assets/frontend/img/DSC05194.JPG" class="img-fluid lozad" alt="">
+                <div class="portfolio-info">
+                <h4>Dharma Jaya</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-video">
+                <a href="galeri-detail.html" style="text-decoration:none;">
+                <img src="<?php base_url();?>/assets/frontend/img/DSC05194.JPG" class="img-fluid lozad" alt="">
+                <div class="portfolio-info">
+                <h4>Dharma Jaya</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
+            <a href="galeri-detail.html" style="text-decoration:none;">
+                <img src="<?php base_url();?>/assets/frontend/img/DSC05194.JPG" class="img-fluid lozad" alt="">
+                <div class="portfolio-info">
+                <h4>Dharma Jaya</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </a>
+            </div>
+        </div>
+        </div>
+    </section>
+    </div>
+    <div class="widget bg-img-bw py-5">
+    <center>
+        <h2 class="container" style="color: yellow;font-weight: bold;">Laporan Pengumpulan ZIS</h2>
+    </center>
+    <section id="laporan">
+        <div class="laporan container">
+        <p class="pt-3">Periode 03 Januari 2022 - 04 Juli 2022</p>
+        <table class="table table-hover">
+            <tr>
+                <th>Zakat</th>
+                <td class="color-green"><a href="laporan.html" style="color:white"> Rp 61.013.418.918</a></td>
+            </tr>
+            <tr>
+                <th>Infak</th>
+                <td class="color-green"><a href="laporan.html"> Rp 32.993.551.086</a></td>
+            </tr>
+            <tr>
+                <th>Total</th>
+                <td class="color-green"><a href="laporan.html"> Rp 94.006.970.004</a></td>
+            </tr>
+        </table>
+        <p>Pembagian Zakat per Wilayah</p>
+        <table class="table table-hover">
+            <thead class="table-head">
+            <tr>
+                <th scope="col"><b>Wilayah</b></th>
+                <th scope="col"><b>Zakat</b></th>
+                <th scope="col"><b>Infak</b></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Provinsi</a></th>
+                <td class="color-green"><a href="laporan.html">Rp 15.631.381.425</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  2.958.845.691</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZNAS (BAZIS) Jakarta Pusat</a></th>
+                <td class="color-green"><a href="laporan.html">Rp  5.603.885.948</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  3.520.785.405</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Utara</a></th>
+                <td class="color-green"><a href="laporan.html">Rp  4.318.168.863</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  3.643.880.132</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Barat</a></th>
+                <td class="color-green"><a href="laporan.html">Rp  8.477.226.601</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  5.080.903.720</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Selatan</a></th>
+                <td class="color-green"><a href="laporan.html">Rp 12.434.109.961</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  7.859.509.784</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Timur</a></th>
+                <td class="color-green"><a href="laporan.html">Rp 13.773.962.961</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp  8.176.940.338</a></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Adm. Kep. Seribu</a></th>
+                <td class="color-green"><a href="laporan.html">Rp    771.683.159</a></td>
+                <td class="color-yellow"><a href="laporan.html">Rp    302.666.016</a></td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+    </section>
+    </div>
+    <div class="widget">
+    <!-- <h2 class="widget-title">Kerjasama</h2> -->
+    <section id="kerjasama">
+        <div class="kerjasama">
+            <center>
+            <a href="https://www.megasyariah.co.id/" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/bank-mega-syariah.png" alt=""></a>
+            <a href="https://www.izi.or.id" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/izi.png" alt=""></a>
+            <a href="https://www.mandiriamalinsani.or.id" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/ymai.png" alt=""></a>
+            <a href="https://www.kitabisa.com" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/kitabisa.png" alt=""></a>
+            <a href="https://www.pln.co.id" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/pln.png" alt=""></a>
+            <a href="https://www.kurmamedia.com" target="_blank"><img src="<?php base_url();?>/assets/frontend/img/kurmamedia.png" alt=""></a>
+            </center>
+        </div>
+    </section>
+    </div>
+    <div class="widget container">
+    <h2 class="widget-title">Zakat Online</h2>
+    <section id="zakat-online">
+        <div class="zakat">
+        <div class="row">
+            <div class="form-group mt-3">
+                <label>Zakat Anda akan disalurkan untuk program-program yang telah disiapkan oleh Baznas Bazis DKI Jakarta</label>                  
+            </div>
+            <div class="input-group mt-3">
+            <input type="text" class="form-control" >
+            <div class="input-group-append">
+                <button class="btn btn-warning" type="button">Zakat</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    </div>
+</div>
+
+<?php $this->load->view('frontend/home/modal_muzakki'); ?>
---
+++ application/views/frontend/home/modal_muzakki.php
@@ -0,0 +1,56 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Rincian Jumlah Mustahik</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+        <table class="table table-hover">
+            <thead class="table-head">
+            <tr>
+                <th scope="col">Wilayah</th>
+                <th scope="col">Jumlah</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Provinsi</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZNAS (BAZIS) Jakarta Pusat</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Utara</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Barat</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Selatan</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Jakarta Timur</a></th>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">BAZINAS (BAZIS) Adm. Kep. Seribu</a></th>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+    </div>
+</div>
