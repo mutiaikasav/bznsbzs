adcd47c5bfc41bd806998b9a842488008656aa37
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-09 01:25:03 +0700

    edit hapus headline

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -30,49 +30,78 @@ class Gallery extends CI_Controller
     public function save()
     {
         $id = $this->input->post('id');
-        if (!empty($_FILES['content_gallery']['name'])) {
-            # code...
-            $config['upload_path']          = './assets/img/galeri';
-            $config['allowed_types']        = 'jpeg|jpg|png';
-            $config['max_size']             = 102400;
-            // $config['max_width']            = 2048;
-            // $config['max_height']           = 1024;
+        $foto[] = $_FILES['content_gallery'];
+        $foto_file_name = array();
+        $i =0;
+        // var_dump($foto);
+        // foreach ($foto as $k => $f) {
+        //     // echo $f[$i];
+        //     $config['upload_path']          = './assets/img/galeri';
+        //     $config['allowed_types']        = 'jpeg|jpg|png';
+        //     $config['max_size']             = 102400;
+
+        //     $this->load->library('upload', $config);
+        //     // echo $f[$i];
+        //     if ( ! $this->upload->do_upload($f['name'][$i]))
+        //     {
+        //         $error = array('error' => $this->upload->display_errors());
+        //         echo $this->upload->display_errors();
+        //     }
+        //     else
+        //     {
+        //         $upload = $this->upload->data();
+        //         array_push($foto_file_name, $upload['file_name']);
+        //     }
+        //     $i++;
+        // }
+
+        // var_dump($foto_file_name);
+        // $arr_foto = implode(", ",$foto);
+        // $data['foto'] = $arr_foto;
+
+        // if (!empty($_FILES['content_gallery']['name'])) {
+        //     # code...
+        //     $config['upload_path']          = './assets/img/galeri';
+        //     $config['allowed_types']        = 'jpeg|jpg|png';
+        //     $config['max_size']             = 102400;
+        //     // $config['max_width']            = 2048;
+        //     // $config['max_height']           = 1024;
     
-            $this->load->library('upload', $config);
+        //     $this->load->library('upload', $config);
     
-            if ( ! $this->upload->do_upload('content_gallery'))
-            {
-                $error = array('error' => $this->upload->display_errors());
-                echo $this->upload->display_errors();
-            }
-            else
-            {
-                $upload = $this->upload->data();
-                $content_gallery = $upload['file_name'];
-            }
-        } else {
-            $content_gallery = $this->input->post('old_content_gallery');
-        }
-        // echo $content_gallery;
-        $data['title_gallery'] = $this->input->post('title_gallery');
-        $data['description_gallery'] = $this->input->post('description_gallery');
-        $data['content_gallery'] = $content_gallery;
-        $data['video'] = $this->input->post('video');
+        //     if ( ! $this->upload->do_array(1) { [0]=> array(5) { ["name"]=> array(1) { [0]=> string(52) "Red and Purple Flower Wedding Reception Card (1).png" } ["type"]=> array(1) { [0]=> string(9) "image/png" } ["tmp_name"]=> array(1) { [0]=> string(14) "/tmp/php6mwOGi" } ["error"]=> array(1) { [0]=> int(0) } ["size"]=> array(1) { [0]=> int(315764) } } }upload('content_gallery'))
+        //     {
+        //         $error = array('error' => $this->upload->display_errors());
+        //         echo $this->upload->display_errors();
+        //     }
+        //     else
+        //     {
+        //         $upload = $this->upload->data();
+        //         $content_gallery = $upload['file_name'];
+        //     }
+        // } else {
+        //     $content_gallery = $this->input->post('old_content_gallery');
+        // }
+        // // echo $content_gallery;
+        // $data['title_gallery'] = $this->input->post('title_gallery');
+        // $data['description_gallery'] = $this->input->post('description_gallery');
+        // $data['content_gallery'] = $content_gallery;
+        // $data['video'] = $this->input->post('video');
         
-        // update
-        if ($id!=null || $id!='') {        
-            $data['updated_at'] = date("Y-m-d H:i:s");
-            $data['updated_by'] = $this->session->userdata('id');
-            $this->gallery_model->update($id, $data);
-            $this->session->set_flashdata('flashSimpan','Data Berhasil disimpan', 'success');
-            redirect(site_url('gallery'));
-        } else { // insert
-            $data['created_at'] = date("Y-m-d H:i:s");
-            $data['created_by'] = $this->session->userdata('id');
-            $this->gallery_model->insert($data);
-            $this->session->set_flashdata('flashSimpan','Data Berhasil disimpan', 'success');
-            redirect(site_url('gallery'));
-        }
+        // // update
+        // if ($id!=null || $id!='') {        
+        //     $data['updated_at'] = date("Y-m-d H:i:s");
+        //     $data['updated_by'] = $this->session->userdata('id');
+        //     $this->gallery_model->update($id, $data);
+        //     $this->session->set_flashdata('flashSimpan','Data Berhasil disimpan', 'success');
+        //     redirect(site_url('gallery'));
+        // } else { // insert
+        //     $data['created_at'] = date("Y-m-d H:i:s");
+        //     $data['created_by'] = $this->session->userdata('id');
+        //     $this->gallery_model->insert($data);
+        //     $this->session->set_flashdata('flashSimpan','Data Berhasil disimpan', 'success');
+        //     redirect(site_url('gallery'));
+        // }
     }
 
     public function edit($id)
