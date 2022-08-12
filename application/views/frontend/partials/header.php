8cc625913f11af0e1cdc2910249156946818b819
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-12 22:18:32 +0700

    meta

--- application/views/frontend/partials/header.php
+++ application/views/frontend/partials/header.php
@@ -1,10 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id-ID">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baznas Bazis DKI Jakarta</title>
+    <meta name="description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta.">
+	<link rel="canonical" href="https://baznasbazisdki.id/" />
+	<meta name="author" content="">
+	<meta name="keywords" content="">
+
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot-news" content="index,follow" />
+    <meta name="googlebot" content="index,follow" />
+
+    <meta name="language" content="id" />
+	<meta name="generator" content="by PT. Prima Infosarana Media" />
+	<meta name="geo.country" content="id" />
+	<meta http-equiv="content-language" content="In-Id" />
+	<meta name="geo.placename" content="Indonesia" />
+	<meta http-equiv="Copyright" content="Baznas Bazis DKI Jakarta" />
+	<meta name="thumbnailUrl" content="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" itemprop="thumbnailUrl" />
+	<meta content="https://baznasbazisdki.id/" itemprop="url" />	
+    <link rel="shortcut icon" href="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" type="image/x-icon">
+	<meta name="content_id" content="" />
+	<meta name="content_site" content="Baznas Bazis DKI Jakarta" />
+	<meta name="content_site_id" content="" />
+	<meta name="content_section_id" content="" />
+	<meta name="content_category" content="" />
+	<meta name="content_location" content="Indonesia" />
+	<meta name="src" content="Baznas Bazis DKI Jakarta" />
+	<meta name="content_subcategory" content="" />
+	<meta name="content_author" content="" />
+	<meta name="content_aid1" content="" />
+	<meta name="content_editor" content="" />
+	<meta name="content_eid" content="" />
+	<meta name="content_lipsus" content="" />
+	<meta name="content_type" content="" />
+	<meta name="content_date" content="" />
+	<meta name="content_tim" content="" />
+	<meta name="content_source" content="" />
+	<meta name="content_tag" content="" />
+	<meta name="content_total_words" content="" />
+	<meta name="content_PublishedDate" content="" />
+	<!-- S:fb meta -->
+	<meta property="og:type" content="article" />
+	<meta property="og:image" content="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" />
+	<meta property="og:title" content="Home - Baznas Bazis DKI Jakarta" />
+	<meta property="og:description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta.">
+	<meta property="og:url" content="https://baznasbazisdki.id/" />
+	<meta property="og:image:alt" content="Baznas Bazis DKI Jakarta" />
+	<meta property="article:author" content="https://www.facebook.com/baznasbazis/" />
+	<meta property="article:publisher" content="https://www.facebook.com/baznasbazis/" />
+    <!-- e:fb meta -->
+
+	<!-- S:tweeter card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@baznasbazis" />
+	<meta name="twitter:creator" content="@baznasbazis">
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta." />
+	<meta name="twitter:image" content="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" />
+	<!-- E:tweeter card -->
 
     <link href="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" rel="icon">
     <link href="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" rel="apple-touch-icon">
