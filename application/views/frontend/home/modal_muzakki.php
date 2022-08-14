e9c7005f055e1332e97660599411b506c0b4a5e6
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-14 22:37:20 +0700

    fix modal muzakki close

--- application/views/frontend/home/modal_muzakki.php
+++ application/views/frontend/home/modal_muzakki.php
@@ -1,12 +1,10 @@
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade" id="exampleModal" tabindex="-1">
+    <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Rincian Jumlah Mustahik</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
+            <h5 class="modal-title" id="exampleModalLabel">Rincian Jumlah Mustahik</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
         <table class="table table-hover">
@@ -49,7 +47,7 @@
         </table>
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         </div>
     </div>
     </div>
