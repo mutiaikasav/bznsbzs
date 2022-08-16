84073a3c9c7d1cd43ac2ca1c55aa6ac1a3994131
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-16 21:12:14 +0700

    search cms view only

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -8,6 +8,19 @@
     </a>
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
+    <form
+        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <div class="input-group">
+            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                aria-label="Search" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                </button>
+            </div>
+        </div>
+    </form>
+    <hr class="sidebar-divider my-0">
     <!-- Nav Item - Dashboard -->
     <li class="nav-item <?php echo ($this->uri->segment(1) == 'dashboard')?'active':''?>">
         <a class="nav-link" href="<?php base_url()?>/dashboard">
