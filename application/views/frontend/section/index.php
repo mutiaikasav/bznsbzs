941176fd39861579703f689d84c58706087dd638
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-16 20:31:09 +0700

    update section fe

--- application/views/frontend/section/index.php
+++ application/views/frontend/section/index.php
@@ -29,36 +29,65 @@
         url: "<?php echo base_url(); ?>api/artikel",
         dataType: 'json',
         success: function(rows)
-        {             
+        {
+            var image_headline = '';
+            // get first image
+            let image_headline1 = rows[0]['content'].split('src="');
+            if (image_headline1.length>1) {
+                let image_headline2 = image_headline1[1].split('" alt=');
+                image_headline = image_headline2[0];
+            }
+            document.querySelector("#section-content").innerHTML = '<div class="headline__wrap clearfix">'+
+                    '<div class="headline__item">'+
+                        '<div class="headline--big tab-content">'+
+                            '<div class="headline__slider fade active in" id="headline1" role="tabpanel" aria-labelledby="home-tab">'+
+                                '<div class="headline__img">'+
+                                    '<a href="<?= base_url()?>artikel/'+rows[0]['id_article']+'/'+rows[0]['slug']+'">'+
+                                        '<img class="lozad" data-src="'+image_headline+'" src="'+image_headline+'" alt="'+rows[0]['title']+'">'+
+                                    '</a>'+
+                                '</div>'+
+                                '<div class="headline__box">'+
+                                    '<div class="headline__channel">'+
+                                        '<span>Kabar Baznas Bazis</span>'+
+                                    '</div>'+
+                                    '<div class="headline__title">'+
+                                        '<h1>'+
+                                            '<a href="<?= base_url()?>artikel/'+rows[0]['id_article']+'/'+rows[0]['slug']+'">'+rows[0]['title']+'</a>'+
+                                        '</h1>'+
+                                    '</div>'+
+                                    '<div class="headline__date">'+rows[0]['published_date']+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>';
             $.each(rows, function (i, item) {
                 var image = '';
-                // get first image
                 let image1 = item['content'].split('src="');
                 if (image1.length>1) {
                     let image2 = image1[1].split('" alt=');
                     image = image2[0];
                 }
-                // kategori filter
-                document.querySelector("#section-content").innerHTML += '<div class="col-lg-4">'+
-                    '<article class="entry">'+
-                        '<div class="entry-img">'+
-                            '<img src='+image+' alt="" class="img-fluid lozad">'+
+                if (i!==0) {
+                    document.querySelector("#section-content").innerHTML += '<div class="latest__item">'+
+                        '<div class="latest__img col-125">'+
+                            '<a href="<?= base_url()?>artikel/'+item['id_article']+'/'+item['slug']+'">'+
+                                '<img class="lozad" data-src="'+image+'" src="'+image+'" alt="'+item['title']+'">'+
+                            '</a>'+
                         '</div>'+
-                        '<h2 class="entry-title">'+
-                            '<a href="<?= base_url()?>artikel/'+item['id_article']+'/'+item['slug']+'">'+item['title']+'</a>'+
-                        '</h2>'+
-                        '<div class="entry-meta">'+
-                        '<ul><li><i class="bi bi-person"></i>Baznas Bazis DKI Jakarta</li></ul>'+
-                        '<ul><li><i class="bi bi-clock"></i><time datetime="2020-01-01">'+item['published_at']+'</time></li></ul>'+
-                        '</div>'+
-                        '<div class="entry-content">'+
-                            '<p></p>'+
-                            '<div class="read-more">'+
-                            '<a href="<?= base_url()?>artikel/'+item['id_article']+'/'+item['slug']+'">Baca Selengkapnya</a>'+
+                        '<div class="latest__box col-80percent">'+
+                            '<div class="latest__channel">'+
+                                '<span> '+title+'</span>'+
+                            '</div>'+
+                            '<div class="latest__title">'+
+                                '<h2><a href="<?= base_url()?>artikel/'+item['id_article']+'/'+item['slug']+'"> '+item['title']+'</a></h2>'+
+                            '</div>'+
+                            '<div class="latest__date">'+item['published_at']+
                             '</div>'+
                         '</div>'+
-                    '</article>'+
-                '</div>';
+                    '</div>';
+                }
             })
         },
         error:function()
@@ -68,9 +97,10 @@
     });
 </script>
 <div class="content">
-    <div class="break-img">
+    <!-- <div class="break-img">
     <img src="<?= base_url(); ?>assets/frontend/img/DSC05194.JPG" alt="" class="lozad">
-    </div>
+    </div> -->
+    <div class="bg-nav"></div>
     <!-- ======= Breadcrumbs ======= -->
     <section id="breadcrumbs" class="breadcrumbs">
         <div class="container">
@@ -82,9 +112,10 @@
     </section>
     <!-- End Breadcrumbs -->
     <section id="blog" class="blog">
-        <div class="container" data-aos="fade-up">      
-            <div class="row" id="section-content" >                
-                <div class="d-flex col-lg-12"></div>
+        <div class="container" data-aos="fade-up">
+            
+            <!-- latest item -->
+            <div class="row col-lg-8" id="section-content" >
                 <!-- End blog entry -->
             </div>
             <div class="blog-pagination">
