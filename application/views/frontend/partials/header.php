37bb15ed2d3d87ec0a9e79f226e7f68e4a46f6af
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-08-19 10:44:46 +0700

    stylesheet cdn to server

--- application/views/frontend/partials/header.php
+++ application/views/frontend/partials/header.php
@@ -67,12 +67,12 @@
     <link href="<?php base_url();?>/assets/frontend/img/Logo Baznas Bazis_2019_v2.png" rel="apple-touch-icon">
 
     <link rel="stylesheet" href="<?php base_url();?>/assets/frontend/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?php base_url();?>/assets/frontend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php base_url();?>/assets/frontend/css/bootstrap-icons.min.css">
     
     <!-- Vendor CSS Files -->
     <link href="<?php base_url();?>/assets/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 
     <script src="<?php base_url(); ?>/assets/vendor/jquery/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php base_url();?>/assets/frontend/css/datatables.min.css"/>
 </head>
