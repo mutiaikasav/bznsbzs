1250fee203770fa764112cfb0afd8cc2a4975906
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-09-02 14:49:57 +0700

    404 fe

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,97 +1,147 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id-ID">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baznas Bazis DKI Jakarta</title>
+    <meta name="description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta.">
+	<link rel="canonical" href="https://baznasbazisdki.id/" />
+	<meta name="author" content="">
+	<meta name="keywords" content="">
 
-    <link href="../../assets/frontend/img/Logo Baznas Bazis_2019_v2.png" rel="icon">
-    <link href="../../assets/frontend/img/Logo Baznas Bazis_2019_v2.png" rel="apple-touch-icon">
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot-news" content="index,follow" />
+    <meta name="googlebot" content="index,follow" />
 
-    <link rel="stylesheet" href="../../assets/frontend/css/style.css">
+    <meta name="language" content="id" />
+	<meta name="generator" content="by PT. Prima Infosarana Media" />
+	<meta name="geo.country" content="id" />
+	<meta http-equiv="content-language" content="In-Id" />
+	<meta name="geo.placename" content="Indonesia" />
+	<meta http-equiv="Copyright" content="Baznas Bazis DKI Jakarta" />
+	<meta name="thumbnailUrl" content="../assets/img/logo-dark-min.png" itemprop="thumbnailUrl" />
+	<meta content="https://baznasbazisdki.id/" itemprop="url" />	
+    <link rel="shortcut icon" href="../assets/img/logo-dark-min.png" type="image/x-icon">
+	<meta name="content_id" content="" />
+	<meta name="content_site" content="Baznas Bazis DKI Jakarta" />
+	<meta name="content_site_id" content="" />
+	<meta name="content_section_id" content="" />
+	<meta name="content_category" content="" />
+	<meta name="content_location" content="Indonesia" />
+	<meta name="src" content="Baznas Bazis DKI Jakarta" />
+	<meta name="content_subcategory" content="" />
+	<meta name="content_author" content="" />
+	<meta name="content_aid1" content="" />
+	<meta name="content_editor" content="" />
+	<meta name="content_eid" content="" />
+	<meta name="content_lipsus" content="" />
+	<meta name="content_type" content="" />
+	<meta name="content_date" content="" />
+	<meta name="content_tim" content="" />
+	<meta name="content_source" content="" />
+	<meta name="content_tag" content="" />
+	<meta name="content_total_words" content="" />
+	<meta name="content_PublishedDate" content="" />
+	<!-- S:fb meta -->
+	<meta property="og:type" content="article" />
+	<meta property="og:image" content="../assets/img/logo-dark-min.png" />
+	<meta property="og:title" content="Home - Baznas Bazis DKI Jakarta" />
+	<meta property="og:description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta.">
+	<meta property="og:url" content="https://baznasbazisdki.id/" />
+	<meta property="og:image:alt" content="Baznas Bazis DKI Jakarta" />
+	<meta property="article:author" content="https://www.facebook.com/baznasbazis/" />
+	<meta property="article:publisher" content="https://www.facebook.com/baznasbazis/" />
+    <!-- e:fb meta -->
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
-    <link href="../../assets/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    </head>
+	<!-- S:tweeter card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@baznasbazis" />
+	<meta name="twitter:creator" content="@baznasbazis">
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:description" content="Badan Amil Zakat Nasional Provinsi DKI Jakarta atau Baznas (Bazis) merupakan badan resmi yang dibentuk oleh pemerintah provinsi Jakarta yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) di wilayah Jakarta." />
+	<meta name="twitter:image" content="../assets/img/logo-dark-min.png" />
+	<!-- E:tweeter card -->
+
+    <link href="../assets/img/logo-dark-min.png" rel="icon">
+    <link href="../assets/img/logo-dark-min.png" rel="apple-touch-icon">
+
+    <link rel="stylesheet" href="../assets/frontend/css/style.css">
+    <link rel="stylesheet" href="../assets/frontend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/frontend/css/bootstrap-icons.min.css">
+    <link href="../assets/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">    
+</head>
 <body>
     <!-- Start Navbar -->
-    <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top" id="myNav">
+    <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top navbar-shrink" id="myNav">
         <div class="container">
             <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span></span>
             <span></span>
             <span></span>
             </button>
-            <a class="navbar-brand text-brand" href=""><img src="../../assets/frontend/img/Logo Baznas Bazis_2019_v2.png" alt="Logo Baznas Bazis DKI Jakarta"></a>
+            <a class="navbar-brand text-brand" href="../../"><img src="../../assets/frontend/img/Logo Baznas Bazis_2019_v2.png" alt="Logo Baznas Bazis DKI Jakarta"></a>
 
             <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
             <ul class="navbar-nav">
-
-                <li class="nav-item">
-                <a href="" class="nav-link active">Beranda</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang</a>
+                    <div class="dropdown-menu">
+                        <a href="../../../../legalitas" class="dropdown-item">Legalitas</a>
+                        <a href="../../visimisi" class="dropdown-item">Visi Misi</a>
+                        <a href="../../struktur" class="dropdown-item">Struktur Organisasi</a>
+                        <a href="../../organisasi" class="dropdown-item">Organisasi</a>
+                        <a href="../../sejarah" class="dropdown-item">Sejarah</a>
+                        <a href="../../kebijakan" class="dropdown-item">Kebijakan Privasi</a>
+                    </div>
                 </li>
 
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang</a>
-                <div class="dropdown-menu">
-                    <a href="../../../../legalitas" class="dropdown-item">Legalitas</a>
-                    <a href="../../visimisi" class="dropdown-item">Visi Misi</a>
-                    <a href="../../struktur" class="dropdown-item">Struktur Organisasi</a>
-                    <a href="../../organisasi" class="dropdown-item">Organisasi</a>
-                    <a href="../../sejarah" class="dropdown-item">Sejarah</a>
-                    <a href="../../hubungi-kami" class="dropdown-item">Hubungi Kami</a>
-                    <a href="../../kebijakan" class="dropdown-item">Kebijakan Privasi</a>
-                </div>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program</a>
+                    <div class="dropdown-menu">
+                        <a href="../../program/jak-b-bertaqwa" class="dropdown-item">Jak B Bertaqwa</a>
+                        <a href="../../program/jak-b-cerdas" class="dropdown-item">Jak B Cerdas</a>
+                        <a href="../../program/jak-b-sehat" class="dropdown-item">Jak B Sehat</a>
+                        <a href="../../program/jak-b-green" class="dropdown-item">Jak B Green</a>
+                        <a href="../../program/jak-b-berdaya" class="dropdown-item">Jak B Berdaya</a>
+                    </div>
                 </li>
 
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program</a>
-                <div class="dropdown-menu">
-                    <a href="../../program/jak-b-bertaqwa" class="dropdown-item">Jak B Bertaqwa</a>
-                    <a href="../../program/jak-b-cerdas" class="dropdown-item">Jak B Cerdas</a>
-                    <a href="../../program/jak-b-sehat" class="dropdown-item">Jak B Sehat</a>
-                    <a href="../../program/jak-b-green" class="dropdown-item">Jak B Green</a>
-                    <a href="../../program/jak-b-berdaya" class="dropdown-item">Jak B Berdaya</a>
-                </div>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
+                    <div class="dropdown-menu">
+                        <a href="../../kalkulator" class="dropdown-item">Kalkulator Zakat</a>
+                        <a href="../../rekening" class="dropdown-item">Rekening Zakat</a>
+                        <a href="../../konfirmasi" class="dropdown-item">Konfirmasi Zakat</a>
+                        <a class="dropdown-item dropdown-toggle" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bayar Zakat Wilayah</a>
+                        <ul>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Pusat</a></li>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Utara</a></li>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Barat</a></li>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Selatan</a></li>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Timur</li>
+                            <li><a href="../../bayarzis" class="sub-dropdown-item">Kepulauan Seribu</a></li>
+                        </ul>
+                    </div>
                 </li>
 
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan Muzakki</a>
-                <div class="dropdown-menu">
-                    <a href="../../kalkulator" class="dropdown-item">Kalkulator Zakat</a>
-                    <a href="../../rekening" class="dropdown-item">Rekening Zakat</a>
-                    <a href="../../konfirmasi" class="dropdown-item">Konfirmasi Zakat</a>
-                    <a class="dropdown-item dropdown-toggle" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bayar Zakat Wilayah</a>
-                    <ul>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Pusat</a></li>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Utara</a></li>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Barat</a></li>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Selatan</a></li>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Jakarta Timur</li>
-                        <li><a href="../../bayarzis" class="sub-dropdown-item">Kepulauan Seribu</a></li>
-                    </ul>
-                </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Berita</a>
-                <div class="dropdown-menu">
-                    <a href="../../kategori/kabar-zakat" class="dropdown-item">Kabar Zakat</a>
-                    <a href="../../kategori/inspirasi" class="dropdown-item">Inspirasi</a>
-                    <a href="../../kategori/lainnya" class="dropdown-item">Lainnya</a>
-                </div>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Berita</a>
+                    <div class="dropdown-menu">
+                        <a href="../../kategori/kabar-zakat" class="dropdown-item">Kabar Zakat</a>
+                        <a href="../../kategori/inspirasi" class="dropdown-item">Inspirasi</a>
+                        <a href="../../kategori/lainnya" class="dropdown-item">Lainnya</a>
+                    </div>
                 </li>
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Media</a>
-                <div class="dropdown-menu">
-                    <a href="../../galeri" class="dropdown-item">Galeri</a>
-                    <a href="../../publikasi" class="dropdown-item">Publikasi</a>
-                </div>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Media</a>
+                    <div class="dropdown-menu">
+                        <a href="../../galeri" class="dropdown-item">Galeri</a>
+                        <a href="../../publikasi" class="dropdown-item">Publikasi</a>
+                    </div>
                 </li>
                 <li class="nav-item">
-                <a href="../../login" class="nav-link">Login</a>
+                    <a href="../../hubungi-kami" class="nav-link">Hubungi Kami</a>
                 </li>
             </ul>
             </div>
@@ -132,9 +182,9 @@
             <div class="col-lg-3 col-md-6 footer-links">
                 <h4>Berita dan Artikel</h4>
                 <ul>
-                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Inspirasi (285)</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Kabar Zakat Baznas Bazis (380)</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Lainnya (21)</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Inspirasi</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Kabar Zakat Baznas Bazis</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="kategori.html">Lainnya</a></li>
                 </ul>
             </div>
 
@@ -176,10 +226,8 @@
     </footer>
     <!-- End Footer -->
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="../../assets/frontend/js/main.js"></script>
-
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+    <script src="../assets/frontend/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="../assets/frontend/js/lozad.min.js"></script>
     <script>
         const observer = lozad(); // lazy loads elements with default selector as '.lozad'
         observer.observe();
